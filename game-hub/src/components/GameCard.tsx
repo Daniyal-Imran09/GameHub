93591850e492496c55cdfa9e4b22da69ee205e23
unknown <daniyal.imran@codesorbit.com>
2024-08-22 16:25:43 +0500

    Displaying Platform Icons

--- game-hub/src/components/GameCard.tsx
+++ game-hub/src/components/GameCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Games } from "../Hooks/UseGame";
-import { Card, CardBody, Heading, Hide, Image } from "@chakra-ui/react";
-
+import { Card, CardBody, Heading, Hide, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
 interface Props {
   game: Games;
 }
@@ -11,6 +11,12 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList
+          Platforms={game.parent_platforms.map((p) => p.platform)}
+        />
+        {/* {game.parent_platforms.map(({ platform }) => (
+          <Text>{platform.name}</Text>
+        ))} */}
       </CardBody>
     </Card>
   );
---
+++ game-hub/src/components/PlatformIconList.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Platform } from "../Hooks/UseGame";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { BsGlobe } from "react-icons/bs";
+import { SiNintendo } from "react-icons/si";
+import { IconType } from "react-icons";
+interface Props {
+  Platforms: Platform[];
+}
+const PlatformIconList = ({ Platforms }: Props) => {
+  const iconmap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  return (
+    <>
+      <HStack marginY={1}>
+        {Platforms.map((platform) => (
+          <Icon as={iconmap[platform.slug]} color="gray.500"></Icon>
+        ))}
+      </HStack>
+    </>
+  );
+};
+
+export default PlatformIconList;
