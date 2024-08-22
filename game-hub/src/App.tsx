415c19ad9fe492c263ea81d24bd81fef36d26603
unknown <daniyal.imran@codesorbit.com>
2024-08-22 12:15:15 +0500

    build a responsive layout

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,7 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg={"yellow"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg={"rebeccapurple"}>
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg={"dodgerblue"}>
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
