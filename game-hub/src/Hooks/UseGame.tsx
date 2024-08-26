c7a059840d8b19a9ee8ace7d31a4fa0032ca3542
Daniyal-Imran09 <daniyalimran602@gmail.com>
2024-08-26 12:28:25 +0500

    Unnecessary Code removal in Usegame

--- game-hub/src/Hooks/UseGame.tsx
+++ game-hub/src/Hooks/UseGame.tsx
@@ -17,40 +17,6 @@ export interface Games {
   rating_top: number;
 }
 
-// const UseGame = (game: GameQuery) => {
-//   const [gameslist, setgameslist] = useState<Games[]>([]);
-//   const [Error, SetError] = useState("");
-//   const [isloading, setisloading] = useState(false);
-
-//   useEffect(() => {
-//     const controller = new AbortController();
-//     setisloading(true);
-//     apiClient
-//       .get<Fetchgames>("/games", {
-//         signal: controller.signal,
-//         params: {
-//           genres: game.genre?.id,
-//           platforms: game.platform?.id,
-//           ordering: game.selectedorder,
-//           search: game.searchtext,
-//         },
-//       })
-//       .then((res) => {
-//         setgameslist(res.data.results);
-//         setisloading(false);
-//       })
-//       .catch((err) => {
-//         if (err instanceof CanceledError) return;
-//         SetError(err.message);
-//         setisloading(false);
-//       });
-
-//     return () => controller.abort();
-//   }, [game]);
-
-//   return { gameslist, Error, isloading };
-// };
-
 const useGame = (game: GameQuery) =>
   useInfiniteQuery<FetchDataResponse<Games>, Error>({
     queryKey: ["games", game],
